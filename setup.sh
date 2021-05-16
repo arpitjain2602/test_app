626624dd0b33e975af015180b689c2ce01b89f97
arpitjain2602 <arpit2602@gmail.com>
2021-05-16 14:11:37 +0530

    adding files

---
+++ setup.sh
@@ -0,0 +1,25 @@
+mkdir -p ~/.streamlit/
+  
+echo "\
+[general]\n\
+email = \"qemail@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.tomlmkdir -p ~/.streamlit/
+  
+echo "\
+[general]\n\
+email = \"qemail@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
---
+++ test_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+
+	st.write('### Hey! This is the demo of streamlit')
+
+if __name__ == "__main__":
+	main()
